f88e3b92b70db21708009cfe76ae4e6f7390f8ba
Rahul Salve <rahul.salve@gmail.com>
2019-08-29 14:49:19 +0100

    Ver 1.0 - Defines the basic Terraform azurerm_resource_group

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Ver 1.0 - Defines the basic Terraform azurerm_resource_grou
+# Ver 1.0 - Defines the basic Terraform azurerm_resource_group
 resource "azurerm_resource_group" "RG" {
     name = "TFLearningRG"
     location = "ukwest"
